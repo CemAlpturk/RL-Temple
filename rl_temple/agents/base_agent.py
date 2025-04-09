48f97fd6633f51406cbcc4fae967ad67a197590c
Cem Alpturk <cem.alpturk@gmail.com>
2025-04-09 22:17:19 +0200

    feat: base agent implementation

---
+++ rl_temple/agents/base_agent.py
@@ -0,0 +1,33 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+
+class BaseAgent(ABC):
+    """Abstract base class for all agents."""
+
+    @abstractmethod
+    def select_action(self, state: Any, explore: bool = False) -> Any:
+        """
+        Selects an action given a state.
+        """
+        pass
+
+    @abstractmethod
+    def remember(self, state, action, reward, next_state, done) -> None:
+        """
+        Store transition in replay memory or buffer.
+        """
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        """
+        Perform a learning update.
+        """
+        pass
+
+    def end_episode(self) -> None:
+        """
+        Optional hook: called at the end of an episode.
+        """
+        pass
