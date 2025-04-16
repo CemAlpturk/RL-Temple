d76bc3caad04123142bfb48f5f764838479f7c17
Cem Alpturk <cem.alpturk@gmail.com>
2025-04-16 20:15:17 +0200

    feat: actorcritic model

---
+++ rl_temple/models/actor_critic.py
@@ -0,0 +1,120 @@
+from typing import Sequence
+import gymnasium as gym
+
+import torch
+import torch.nn as nn
+from torch import distributions as dist
+
+from .mlp import MLP
+
+
+class Actor(nn.Module):
+
+    def _distribution(self, obs: torch.Tensor) -> dist.Distribution:
+        raise NotImplementedError
+
+    def _log_prob_from_distribution(
+        self,
+        pi: torch.distributions.Distribution,
+        act: torch.Tensor,
+    ) -> torch.Tensor:
+        raise NotImplementedError
+
+    def forward(
+        self,
+        obs: torch.Tensor,
+        act: torch.Tensor | None = None,
+    ) -> tuple[dist.Distribution, torch.Tensor | None]:
+        pi = self._distribution(obs)
+        logp_a = None
+
+        if act is not None:
+            logp_a = self._log_prob_from_distribution(pi, act)
+        return pi, logp_a
+
+
+class MLPCategoricalActor(Actor):
+
+    def __init__(
+        self,
+        obs_dim: int,
+        act_dim: int,
+        hidden_sizes: Sequence[int],
+        activation: str,
+    ) -> None:
+        super().__init__()
+        self.logits_net = MLP(
+            input_size=obs_dim,
+            output_size=act_dim,
+            hidden_sizes=hidden_sizes,
+            activation=activation,
+        )
+
+    def _distribution(self, obs: torch.Tensor) -> dist.Categorical:
+        logits: torch.Tensor = self.logits_net(obs)
+        return dist.Categorical(logits=logits)
+
+    def _log_prob_from_distribution(
+        self,
+        pi: torch.distributions.Distribution,
+        act: torch.Tensor,
+    ) -> torch.Tensor:
+        return pi.log_prob(act)
+
+
+class MLPCritic(nn.Module):
+
+    def __init__(
+        self,
+        obs_dim: int,
+        hidden_sizes: Sequence[int],
+        activation: str,
+    ) -> None:
+        super().__init__()
+        self.v_net = MLP(
+            input_size=obs_dim,
+            output_size=1,
+            hidden_sizes=hidden_sizes,
+            activation=activation,
+        )
+
+    def forward(self, obs: torch.Tensor) -> torch.Tensor:
+        return torch.squeeze(self.v_net(obs), -1)
+
+
+class MLPActorCritic(nn.Module):
+
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        hidden_sizes: Sequence[int] = (64, 64),
+        activation: str = "tanh",
+    ) -> None:
+
+        super().__init__()
+
+        assert observation_space.shape is not None
+        obs_dim = observation_space.shape[0]
+
+        # Policy builder
+        if isinstance(action_space, gym.spaces.Discrete):
+            act_dim = int(action_space.n)
+            self.pi = MLPCategoricalActor(obs_dim, act_dim, hidden_sizes, activation)
+        else:
+            raise NotImplementedError
+
+        # Value function
+        self.v = MLPCritic(obs_dim, hidden_sizes, activation)
+
+    def step(self, obs: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+
+        with torch.no_grad():
+            pi = self.pi._distribution(obs)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v: torch.Tensor = self.v(obs)
+        return a, v, logp_a
+
+    def act(self, obs: torch.Tensor) -> torch.Tensor:
+        return self.step(obs)[0]
