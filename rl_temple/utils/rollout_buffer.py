4ef0f36d79fce508210a5b0c8a5507c9feae6791
Cem Alpturk <cem.alpturk@gmail.com>
2025-05-17 16:57:04 +0300

    feat: store multiple episodes in rollout buffer

--- rl_temple/utils/rollout_buffer.py
+++ rl_temple/utils/rollout_buffer.py
@@ -1,30 +1,53 @@
+import numpy as np
+
+Transition = tuple[np.ndarray, np.ndarray, float, float, float, bool]
+
+
 class RolloutBuffer:
 
     def __init__(self) -> None:
         self.clear()
 
-    def add(self, transition: tuple) -> None:
-        self.states.append(transition[0])
-        self.actions.append(transition[1])
-        self.rewards.append(transition[2])
-        self.log_probs.append(transition[3])
-        self.values.append(transition[4])
-        self.dones.append(transition[5])
+    def add(self, transition: Transition) -> None:
+        state, action, reward, log_prob, value, done = transition
 
-    def get(self) -> tuple:
-        return (
-            self.states,
-            self.actions,
-            self.rewards,
-            self.log_probs,
-            self.values,
-            self.dones,
-        )
+        self.current_states.append(state)
+        self.current_actions.append(action)
+        self.current_rewards.append(reward)
+        self.current_log_probs.append(log_prob)
+        self.current_values.append(value)
+        self.current_dones.append(done)
+
+    def end_episode(self) -> None:
+        episode = {
+            "states": np.array(self.current_states),
+            "actions": np.array(self.current_actions),
+            "rewards": np.array(self.current_rewards),
+            "log_probs": np.array(self.current_log_probs),
+            "values": np.array(self.current_values),
+            "dones": np.array(self.current_dones),
+        }
+        self.trajectories.append(episode)
+
+        self.current_states = []
+        self.current_actions = []
+        self.current_rewards = []
+        self.current_log_probs = []
+        self.current_values = []
+        self.current_dones = []
+
+    def get(self) -> list[dict[str, np.ndarray]]:
+        return self.trajectories
 
     def clear(self) -> None:
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.log_probs = []
-        self.values = []
-        self.dones = []
+        self.trajectories: list[dict[str, np.ndarray]] = []
+
+        self.current_states: list[np.ndarray] = []
+        self.current_actions: list[np.ndarray] = []
+        self.current_rewards: list[float] = []
+        self.current_log_probs: list[float] = []
+        self.current_values: list[float] = []
+        self.current_dones: list[bool] = []
+
+    def __len__(self) -> int:
+        return len(self.trajectories)
